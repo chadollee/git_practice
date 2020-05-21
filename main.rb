da462caaacc2141a7572c1df281f9dc01c22a49e
chadol lee <chadollee@protonmail.com>
2020-05-20 19:29:48 -0700

    changes

--- main.rb
+++ main.rb
@@ -1,20 +1,23 @@
+require 'colorize'
 def puts_git(cmd)
   puts `git #{cmd} -h`
   menu
 end
 def menu
-  puts '1: Enter git command'
-  puts '2: Exit'
+  puts 'Main Menu'.colorize(:red)
+  puts '1: Enter git command'.colorize(:green)
+  puts '2: Exit'.colorize(:red)
   choice = gets.to_i
   case choice
   when 1
+    puts 'Enter Here:'
     puts 'Enter git command'
     puts_git(gets.strip)
     menu
   when 2
     exit
   else
-    puts 'Invalid choice'
+    # puts 'Invalid choice'
     menu
   end
 end
